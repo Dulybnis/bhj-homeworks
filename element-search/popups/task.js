5a04972b85c9bee654f7dd2fce5e1de76c179887
dulybnis <ddn555@yandex.ru>
2022-09-06 22:57:56 +0500

    edited solution

--- element-search/popups/task.js
+++ element-search/popups/task.js
@@ -1,18 +1,17 @@
 let popup = document.getElementById("modal_main");
 let sucess = document.getElementById("modal_success");
+let closeClick = [];
 popup.classList = "modal modal_active";
 
-const close = document.getElementsByClassName("modal__close");
-close[0].onclick = function() {
-    popup.classList = "modal";
-};
+const close = Array.from(document.getElementsByClassName("modal__close"));
+close.forEach((element) => {
+    closeClick.push(element.onclick = function() {
+        element.parentElement.parentElement.classList = "modal";
+    })
+});
 
 const show = document.getElementsByClassName("show-success");
 show[0].onclick = function() {
     sucess.classList = "modal modal_active";
     popup.classList = "modal";
 }
-
-close[2].onclick = function() {
-    sucess.classList = "modal";
-};
--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -2,8 +2,27 @@ let left = document.getElementsByClassName("slider__arrow_prev")[0];
 let right = document.getElementsByClassName("slider__arrow_next")[0];
 let images = Array.from(document.getElementsByClassName("slider__item"));
 let dots = Array.from(document.getElementsByClassName("slider__dot"));
-let active = images.findIndex((item) => item.className === "slider__item slider__item_active");
+let active = 0;
+let exActive = 0;
 dots[active].className = "slider__dot slider__dot_active";
+
+let validateSlide = function(number) {
+    if (number > images.length - 1) {
+        return 0;
+    };
+    if (number < 0) {
+        return images.length - 1;
+    };
+    return number
+}
+
+let activeSlide = function(number) {
+    active = validateSlide(number);
+    images[exActive].className = "slider__item";
+    dots[exActive].className = "slider__dot";
+    images[active].className = "slider__item slider__item_active";
+    dots[active].className = "slider__dot slider__dot_active";
+}
 
 let clickDots = dots.forEach((element, index) => {
     element.onclick = function() {
@@ -11,34 +30,20 @@ let clickDots = dots.forEach((element, index) => {
             console.log("Уже активен!");
             return;
         } else {
-            dots.forEach((element) => {
-                element.className = "slider__dot";
-            })
-            element.className = "slider__dot slider__dot_active";
-            active = images.findIndex((item) => item.className === "slider__item slider__item_active");
-            images[active].className = "slider__item";
-            images[index].className = "slider__item slider__item_active";
+            exActive = active;
+            activeSlide(index);
         }
-
     }
 });
 
 left.onclick = function() {
-    active = images.findIndex((item) => item.className === "slider__item slider__item_active");
-    let prev = (active === 0) ? images.length - 1 : active - 1;
-    images[active].className = "slider__item";
-    images[prev].className = "slider__item slider__item_active";
-    dots[active].className = "slider__dot";
-    dots[prev].className = "slider__dot slider__dot_active";
-    active = prev;
+    exActive = active;
+    active -= 1;
+    activeSlide(active);
 }
 
 right.onclick = function() {
-    active = images.findIndex((item) => item.className === "slider__item slider__item_active");
-    let next = (active === images.length - 1) ? 0: active + 1;
-    images[active].className = "slider__item";
-    images[next].className = "slider__item slider__item_active";
-    dots[active].className = "slider__dot";
-    dots[next].className = "slider__dot slider__dot_active";
-    active = next;
+    exActive = active;
+    active += 1;
+    activeSlide(active);
 }
